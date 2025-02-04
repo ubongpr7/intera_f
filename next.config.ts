ad8adac1894ff84f6e1f105924dadee7cab3d6ed
UbongPr7 <ubongpr7@gmail.com>
2025-02-04 16:28:04 +0000

    deployment fixes

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config, { isServer }) {
-    // Handling CSS imports in your Next.js project
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    });
-
-    return config;
-  },
-
-  // Optional: If you want to enable React Strict Mode (recommended for new apps)
-  reactStrictMode: true,
-
-  // Add custom Webpack configuration here if needed
+  /* config options here */
 };
 
 export default nextConfig;
