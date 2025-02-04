b7eae5d837773e7647017f786deffde583699266
UbongPr7 <ubongpr7@gmail.com>
2025-02-04 10:13:58 +0000

    eslint rules

--- components/utils/Setup.tsx
+++ components/utils/Setup.tsx
@@ -1,11 +1,11 @@
-'use client';
+// 'use client';
 
-import { useVerify } from '@/hooks';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import { useVerify } from '@/hooks';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
-export default function Setup() {
-	useVerify();
+// export default function Setup() {
+// 	useVerify();
 
-	return <ToastContainer />;
-}
+// 	return <ToastContainer />;
+// }
