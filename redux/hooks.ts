33b87603117e431e324aa7cab36a34c964dfee2e
UbongPr7 <ubongpr7@gmail.com>
2025-02-04 10:23:34 +0000

    deployment issues

--- redux/hooks.ts
+++ redux/hooks.ts
@@ -1,10 +1,23 @@
-import {useSelector,UseDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { AppDispatch, RootState } from './store';
-import {TypedUseSelectorHook} from 'react-redux';
-import { use } from 'react';
+import { TypedUseSelectorHook } from 'react-redux';
 
-export const UseAppDispatch:()=>AppDispatch=UseAppDispatch;
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:269971561.
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:4289991333.
-export const useAppSelector:TypedUseSelectorHook<RootState>=useSelector;
-export const useAppDispatch:()=>AppDispatch=useDispatch;
+// This is the correct way to define useAppDispatch
+export const useAppDispatch: () => AppDispatch = useDispatch;
+
+// This is the correct way to define useAppSelector
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+
+// import {useSelector,UseDispatch} from 'react-redux';
+// import { AppDispatch, RootState } from './store';
+// import {TypedUseSelectorHook} from 'react-redux';
+// import { use } from 'react';
+
+// export const UseAppDispatch:()=>AppDispatch=UseAppDispatch;
+// // Suggested code may be subject to a license. Learn more: ~LicenseLog:269971561.
+// // Suggested code may be subject to a license. Learn more: ~LicenseLog:4289991333.
+// export const useAppSelector:TypedUseSelectorHook<RootState>=useSelector;
+// export const useAppDispatch:()=>AppDispatch=useDispatch;
+
+
