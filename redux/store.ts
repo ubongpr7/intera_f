3326a0e8fc280c524efbc441e2f2c15ea4c8f567
UbongPr7 <ubongpr7@gmail.com>
2025-02-04 10:30:28 +0000

    deployment issues

--- redux/store.ts
+++ redux/store.ts
@@ -1,18 +1,37 @@
-import {configureStore} from "@reduxjs/toolkit";
+// import {configureStore} from "@reduxjs/toolkit";
+// import authReducer from "./features/authSlice";
+// import { apiSlice } from "./services/apiSlice";
+// export const makeStore = ()=>
+//     configureStore({
+//         reducer:{
+//             [apiSlice.reducerPath]:apiSlice.reducer,
+//         },
+        
+//         auth: authReducer,
+//         middleware: getDefaultMiddleware =>
+//             getDefaultMiddleware().concat(apiSlice.middleware),
+//         devtools:process.NODE_ENV !=='production'
+//     })
+
+//     export type AppStore =ReturnType<typeof makeStore>;
+//     export type RootState = ReturnType<AppStore['getState']>;
+//     export type AppDispatch = AppStore['dispatch'];
+
+import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
 import { apiSlice } from "./services/apiSlice";
-export const makeStore = ()=>
-    configureStore({
-        reducer:{
-            [apiSlice.reducerPath]:apiSlice.reducer,
-        },
-        
-        auth: authReducer,
-        middleware: getDefaultMiddleware =>
-            getDefaultMiddleware().concat(apiSlice.middleware),
-        devtools:process.NODE_ENV !=='production'
-    })
 
-    export type AppStore =ReturnType<typeof makeStore>;
-    export type RootState = ReturnType<AppStore['getState']>;
-    export type AppDispatch = AppStore['dispatch'];
+export const makeStore = () =>
+  configureStore({
+    reducer: {
+      [apiSlice.reducerPath]: apiSlice.reducer,
+      auth: authReducer,  // Make sure auth is included here
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(apiSlice.middleware),
+    devtools: process.env.NODE_ENV !== "production", // Correct usage of process.env
+  });
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
