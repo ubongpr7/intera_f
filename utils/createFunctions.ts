e90e8dcd6351cb5fb5cebca7fc2b3f7c258fe782
UbongPr7 <ubongpr7@gmail.com>
2025-02-05 12:55:36 +0000

    Ads Account creation

--- utils/createFunctions.ts
+++ utils/createFunctions.ts
@@ -1,22 +1,33 @@
 import { useCreateAdAccountMutation } from "@/redux/features/adAccountApiSlice"; 
 import { toast } from "react-toastify";
 
-export const   CreateAdAccountComponent = (access_token:string,ad_account_id:string) => {
-    const [createAdAccount, { isLoading, isError, data }] = useCreateAdAccountMutation();
+interface Props {
+  access_token: string;
+  ad_account_id: string;
+}
 
-    const handleCreate = async () => {
-        try {
-            const newAdAccount = {
-                ad_account_id:ad_account_id,
-                access_token: ad_account_id,
-                
-            };
-            await createAdAccount(newAdAccount).unwrap();
+const CreateAdAccountComponent: React.FC<Props> = ({ access_token, ad_account_id }) => {
+  const [createAdAccount, { isLoading }] = useCreateAdAccountMutation();
 
-            toast.success('Account Setup Successful!');
-        } catch (error) {
-            console.error("Failed to create ad account:", error);
-        }
-    };
+  const handleCreate = async () => {
+    try {
+      const newAdAccount = {
+        ad_account_id,
+        access_token,
+      };
+      await createAdAccount(newAdAccount).unwrap();
+      toast.success("Account Setup Successful!");
+    } catch (error) {
+      console.error("Failed to create ad account:", error);
+      toast.error("Failed to create ad account.");
+    }
+  };
 
+  return (
+    <button onClick={handleCreate} disabled={isLoading}>
+      {isLoading ? "Creating..." : "Create Ad Account"}
+    </button>
+  );
 };
+
+export default CreateAdAccountComponent;
