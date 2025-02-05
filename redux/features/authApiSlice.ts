a70808256257840007f3f5be1d6ce6a697e4f387
UbongPr7 <ubongpr7@gmail.com>
2025-02-05 13:38:12 +0000

    Ads Account creation

--- redux/features/authApiSlice.ts
+++ redux/features/authApiSlice.ts
@@ -39,7 +39,7 @@ const authApiSlice = apiSlice.injectEndpoints({
 		}),
 		login: builder.mutation({
 			query: ({ email, password }) => ({
-				url: '/auth-token/jwt/create/',
+				url: '/jwt/create/',
 				method: 'POST',
 				body: { email, password },
 			}),
@@ -59,7 +59,7 @@ const authApiSlice = apiSlice.injectEndpoints({
 		}),
 		verify: builder.mutation({
 			query: () => ({
-				url: '/auth-token/jwt/verify/',
+				url: '/jwt/verify/',
 				method: 'POST',
 			}),
 		}),
--- redux/hooks.ts
+++ redux/hooks.ts
@@ -2,22 +2,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { AppDispatch, RootState } from './store';
 import { TypedUseSelectorHook } from 'react-redux';
 
-// This is the correct way to define useAppDispatch
 export const useAppDispatch: () => AppDispatch = useDispatch;
 
-// This is the correct way to define useAppSelector
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-
-// import {useSelector,UseDispatch} from 'react-redux';
-// import { AppDispatch, RootState } from './store';
-// import {TypedUseSelectorHook} from 'react-redux';
-// import { use } from 'react';
-
-// export const UseAppDispatch:()=>AppDispatch=UseAppDispatch;
-// // Suggested code may be subject to a license. Learn more: ~LicenseLog:269971561.
-// // Suggested code may be subject to a license. Learn more: ~LicenseLog:4289991333.
-// export const useAppSelector:TypedUseSelectorHook<RootState>=useSelector;
-// export const useAppDispatch:()=>AppDispatch=useDispatch;
-
 
