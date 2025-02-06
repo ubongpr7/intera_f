4a3511eb15b150ba8426d7d6decc5b6c13bd04cb
UbongPr7 <ubongpr7@gmail.com>
2025-02-06 21:35:09 +0000

    eslint rules

--- utils/refreshAuthClient.ts
+++ utils/refreshAuthClient.ts
@@ -1,27 +1,27 @@
-// components/TokenRefresher.tsx
-'use client';
+// // components/TokenRefresher.tsx
+// 'use client';
 
-import { useEffect } from 'react';
-import { useRouter, usePathname } from 'next/navigation';
-import { getCookie } from 'cookies-next';
-import { refreshUserToken } from './refreshAuth';
+// import { useEffect } from 'react';
+// import { useRouter, usePathname } from 'next/navigation';
+// import { getCookie } from 'cookies-next';
+// import { refreshUserToken } from './refreshAuth';
 
-export default function TokenRefresher() {
-  const router = useRouter();
-  const pathname = usePathname();
-  const refreshToken = getCookie('refreshToken');
+// export default function TokenRefresher() {
+//   const router = useRouter();
+//   const pathname = usePathname();
+//   const refreshToken = getCookie('refreshToken');
 
-  useEffect(() => {
-    const refresh = async () => {
-      await refreshUserToken(refreshToken, pathname, router);
-    };
+//   useEffect(() => {
+//     const refresh = async () => {
+//       await refreshUserToken(refreshToken, pathname, router);
+//     };
 
-    refresh();
+//     refresh();
 
-    const interval = setInterval(refresh, 40 * 60 * 1000); // 40 minutes
+//     const interval = setInterval(refresh, 40 * 60 * 1000); // 40 minutes
 
-    return () => clearInterval(interval);
-  }, [refreshToken, pathname, router]);
+//     return () => clearInterval(interval);
+//   }, [refreshToken, pathname, router]);
 
-  return null; 
-}
+//   return null; 
+// }
