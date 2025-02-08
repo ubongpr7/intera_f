2b6468069de3de7fa7f74cbf84740e2880f82a69
UbongPr7 <ubongpr7@gmail.com>
2025-02-08 18:40:43 +0000

    Dashboard logic

--- redux/features/adsManagerApiSlice.ts
+++ redux/features/adsManagerApiSlice.ts
@@ -82,22 +82,58 @@ const adManagerApiSlice = apiSlice.injectEndpoints({
         }),
 
         // AdSet Endpoints
-        createAdSet: builder.mutation<any, FormData>({
-            query: (formData) => {
-                const dataObject = {};
-        
-                // Convert FormData to a plain object
-                formData.forEach((value, key) => {
-                    dataObject[key] = value;
-                });
-        
-                return {
-                    url: `/${ads_manager_api}/adsets/create/`,
-                    method: "POST",
-                    body: dataObject,  // Send as a JSON object
-                };
-            },
+        createAdSet: builder.mutation({
+            query: ({
+                ad_creative_description,
+                ad_creative_headline,
+                ad_creative_primary_text,
+                ad_format,
+                attribution_setting,
+                audience_network,
+                call_to_action,
+                destination_url,
+                facebook,
+                facebook_reels,
+                feeds,
+                gender,
+                instagram,
+                instagram_feeds,
+                instagram_profile_feed,
+                instagram_reels,
+                instagram_search,
+                instagram_stories,
+                native_banner_interstitial,
+                rewarded_videos,
+                url_parameters
+            }) => ({
+                url: `/${ads_manager_api}/adsets/create/`,
+                method: "POST",
+                body: {
+                    ad_creative_description,
+                    ad_creative_headline,
+                    ad_creative_primary_text,
+                    ad_format,
+                    attribution_setting,
+                    audience_network,
+                    call_to_action,
+                    destination_url,
+                    facebook,
+                    facebook_reels,
+                    feeds,
+                    gender,
+                    instagram,
+                    instagram_feeds,
+                    instagram_profile_feed,
+                    instagram_reels,
+                    instagram_search,
+                    instagram_stories,
+                    native_banner_interstitial,
+                    rewarded_videos,
+                    url_parameters
+                },
+            }),
         }),
+        
         
         getAdSets: builder.query<AdSet[], void>({
             query: () => `/${ads_manager_api}/adsets/`,
