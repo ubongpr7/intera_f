3f36bf43cc0d1a44f3caa5d032771204df398935
UbongPr7 <ubongpr7@gmail.com>
2025-03-18 16:49:03 +0000

    Update environment configuration with new backend host URL and remove playground.js file

--- env_file.ts
+++ env_file.ts
@@ -1,11 +1,6 @@
 const env = {
-    BACKEND_HOST_URL: 'http://localhost:8000/',
-    FRONTEND_HOST_URL: 'https://quickcampaigns.io/',
-    RECAPTCHA_SECRET_KEY:'6LdiOKsqAAAAAMt7E8lrjzZ97uIT18WcFVcpnHHg',
-    ENTERPRICE_PRICE_ID:'price_1Qo3xrBslQSg03TDESIIAFM2',
-    EXCLUSIVE_PRICE_ID:'price_1Qo3x1BslQSg03TDWby1mx99',
-    FACEBOOK_APP_ID:'1153977715716035',
-    FACEBOOK_APP_SECRET:'30d73e973e26535fc1e445f2e0b16cb7'
+    BACKEND_HOST_URL: 'http://3.212.68.52:8000/',
+    // BACKEND_HOST_URL: "http://localhost:3000",
 
   };
   
