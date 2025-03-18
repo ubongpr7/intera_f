a958f5d0d4e9d1d04e74da91c655ca876e63541c
UbongPr7 <ubongpr7@gmail.com>
2025-03-18 20:06:30 +0000

    Refactor import paths in layout and page components to use relative paths for consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 // app/page.tsx
-import HeroSection from 'components/landing/HeroSection';
-import FeatureGrid from 'components/landing/FeatureGrid';
-import DashboardPreview from 'components/landing/DashboardPreview';
-import ReportsShowcase from 'components/landing/ReportsShowcase';
-import CTASection from 'components/landing/CTASection';
-import Navbar from 'components/landing/homeNave';
+import HeroSection from '../components/landing/HeroSection';
+import FeatureGrid from '../components/landing/FeatureGrid';
+import DashboardPreview from '../components/landing/DashboardPreview';
+import ReportsShowcase from '../components/landing/ReportsShowcase';
+import CTASection from '../components/landing/CTASection';
+import Navbar from '../components/landing/homeNave';
 
 export default function Home() {
   return (
--- redux/provider.tsx
+++ redux/provider.tsx
@@ -1,17 +1,3 @@
-// 'use client';
-
-// import { makeStore } from "./store";
-// import { Provider } from "react-redux";
-
-// interface Props{
-//     children:React.ReactNode;
-// }
-
-// export default function StoreProvider({children}:Props){
-//     const store = makeStore();
-//     return <Provider store={store}>{children}</Provider>
-// }
-// provider.tsx (minimal changes)
 'use client';
 
 import { makeStore } from "./store";
