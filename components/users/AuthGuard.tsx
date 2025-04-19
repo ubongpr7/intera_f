0c025a80252b00fd7977c9e8caba6fa3ccae600e
UbongPr7 <ubongpr7@gmail.com>
2025-04-19 11:52:06 +0100

    fix: Refactor AuthGuard component to improve readability and maintainability

--- components/users/AuthGuard.tsx
+++ components/users/AuthGuard.tsx
@@ -1,7 +1,9 @@
+import { User } from "lucide-react";
 import { useAuth } from "../../redux/features/users/useAuth";
-
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 export const AuthGuard = ({ children }: { children: React.ReactNode }) => {
-    
+    const router=useRouter()
     const { isLoading, isAuthenticated, isPublic } = useAuth();
   
     if (isLoading) {
@@ -10,10 +12,15 @@ export const AuthGuard = ({ children }: { children: React.ReactNode }) => {
         </div>
       );
     }
-  
+    
     if (!isPublic && !isAuthenticated) {
       return null; // Redirect already handled in useAuth
     }
-  
+    useEffect(() => {
+      if (!isLoading && isPublic && isAuthenticated) {
+          router.push('/dashboard');
+      }
+  }, [isLoading, isPublic, isAuthenticated, router]);
+
     return <>{children}</>;
   };
