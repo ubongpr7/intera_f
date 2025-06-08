b88062fbe420bee4a872e933720a6f841ae6ce46
UbongPr7 <ubongpr7@gmail.com>
2025-06-09 00:18:09 +0100

    Refactor API slice: enhance file upload handling and improve token management

--- hooks/useCompanyProfile.ts
+++ hooks/useCompanyProfile.ts
@@ -1,6 +1,8 @@
 "use client"
 
-import { useCreateCompanyProfileMutation, useGetOwnerCompanyProfileQuery, useUpdateCompanyProfileMutation } from "redux/features/management/companyProfileApiSlice"
+import { useCreateCompanyProfileMutation,
+   useGetOwnerCompanyProfileQuery, 
+   useUpdateCompanyProfileMutation } from "@/redux/features/management/companyProfileApiSlice"
 
 
 
