e3ccfa65603580ef5ca65778e26df0d0e674920f
UbongPr7 <ubongpr7@gmail.com>
2025-07-13 16:06:36 +0100

    feat: Enhance DataTable with row numbering and customizable header options

--- env_file.ts
+++ env_file.ts
@@ -1,8 +1,8 @@
 const env = {
   COMMON_BACKEND_URL: "http://localhost:7000/",
     BACKEND_HOST_URL: "https://dev.users.destinybuilders.africa/",
-    INVENNTORY_BACKEND_URL: "http://localhost:8002/",
-    PRODUCT_BACKEND_URL: "http://localhost:8003/",
+    INVENNTORY_BACKEND_URL: "https://dev.inventory.destinybuilders.africa/",
+    PRODUCT_BACKEND_URL: "https://dev.product.destinybuilders.africa/",
     POS_BACKEND_URL: "http://localhost:8004/",
   };
   
