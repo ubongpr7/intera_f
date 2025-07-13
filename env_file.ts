4b967e3fd91cbb07912102bbecb61d6233f1b350
UbongPr7 <ubongpr7@gmail.com>
2025-07-13 13:34:04 +0100

    feat: Enhance DataTable with row numbering and customizable header options

--- env_file.ts
+++ env_file.ts
@@ -1,10 +1,10 @@
 const env = {
   COMMON_BACKEND_URL: "http://localhost:7000/",
-    BACKEND_HOST_URL: "http://localhost:8001/",
+    BACKEND_HOST_URL: "https://dev.users.destinybuilders.africa/admin/accounts/verificationcode/",
     INVENNTORY_BACKEND_URL: "http://localhost:8002/",
     PRODUCT_BACKEND_URL: "http://localhost:8003/",
     POS_BACKEND_URL: "http://localhost:8004/",
   };
   
-  export default env;
+export default env;
   
