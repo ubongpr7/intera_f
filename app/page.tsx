ce5427af6c83d7571adddcd6391e27dbcd6acd0b
UbongPr7 <ubongpr7@gmail.com>
2025-08-12 17:58:33 +0100

    css for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import './home.css';
+import "./home.css";
 import { useState, useEffect, useRef } from "react"
 import { motion, useScroll, useTransform, AnimatePresence } from "framer-motion"
 import { Canvas, useFrame } from "@react-three/fiber"
