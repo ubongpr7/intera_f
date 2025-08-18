43b6ed717250426147872dc51965bcf60dec8789
UbongPr7 <ubongpr7@gmail.com>
2025-08-18 23:58:10 +0100

    feat: update email validation regex to enforce stronger password criteria

--- components/auth/registerForm.tsx
+++ components/auth/registerForm.tsx
@@ -106,7 +106,7 @@ export default function RegisterForm() {
           {...register('email', { 
             required: 'Email is required',
             pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{10,}$/,
               message: 'Invalid email address'
             }
           })}
