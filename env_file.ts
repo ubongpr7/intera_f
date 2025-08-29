2324e40052ae024d72d298d359be5729f9809e36
UbongPr7 <ubongpr7@gmail.com>
2025-08-30 00:33:47 +0100

    real-time dashboard

--- env_file.ts
+++ env_file.ts
@@ -3,7 +3,7 @@ const env = {
     BACKEND_HOST_URL: "https://dev.users.destinybuilders.africa/",
     INVENNTORY_BACKEND_URL: "https://dev.inventory.destinybuilders.africa/",
     PRODUCT_BACKEND_URL: "https://dev.product.destinybuilders.africa/",
-    POS_BACKEND_URL: "http://localhost:8004/",
+    POS_BACKEND_URL: "https://dev.pos.destinybuilders.africa/",
     AGENT_BACKEND_URL: "https://dev.pos.destinybuilders.africa/",
     PAYMENT_BACKEND_URL: "http:localhost:8550/",
   };
