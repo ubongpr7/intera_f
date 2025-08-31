b915ee864f7b96f74c7ebfab015c6e49fdf8ec9e
UbongPr7 <ubongpr7@gmail.com>
2025-08-31 03:25:15 +0100

    real-time dashboard

--- components/realtime-dashboard/QuickStats.tsx
+++ components/realtime-dashboard/QuickStats.tsx
@@ -142,7 +142,7 @@ const QuickStats = () => {
             </span>
           </div>
           
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
             {lowStockData.map((item: any) => (
               <div 
                 key={item.id} 
