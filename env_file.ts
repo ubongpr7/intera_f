9e0782c993b75c9c3270afa0dc3aa81eda0d66a9
UbongPr7 <ubongpr7@gmail.com>
2025-10-03 08:35:00 +0100

    fix: update backend URLs to point to the correct development environment

--- env_file.ts
+++ env_file.ts
@@ -1,10 +1,10 @@
 const env = {
   COMMON_BACKEND_URL: "http://localhost:7000/",
-    BACKEND_HOST_URL: "https://dev.users.destinybuilders.africa/",
-    INVENNTORY_BACKEND_URL: "https://dev.inventory.destinybuilders.africa/",
-    PRODUCT_BACKEND_URL: "https://dev.product.destinybuilders.africa/",
-    POS_BACKEND_URL: "https://dev.pos.destinybuilders.africa/",
-    AGENT_BACKEND_URL: "https://dev.pos.destinybuilders.africa/",
+    BACKEND_HOST_URL: "https://dev.accounts.interaims.com/",
+    INVENNTORY_BACKEND_URL: "https://dev.inventory.interaims.com/",
+    PRODUCT_BACKEND_URL: "https://dev.product.interaims.com/",
+    POS_BACKEND_URL: "https://dev.pos.interaims.com/",
+    AGENT_BACKEND_URL: "https://dev.pos.interaims.com/",
     PAYMENT_BACKEND_URL: "http:localhost:8550/",
   };
 
